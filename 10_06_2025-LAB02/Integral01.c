2bc41f214a606703d36788140b7a071ab7763ffc
Gabriel Matos Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-06-10 11:58:58 -0300

    Update Integral01.c

--- 10_06_2025-LAB02/Integral01.c
+++ 10_06_2025-LAB02/Integral01.c
@@ -17,13 +17,13 @@
     Se D>0 e fxx<0 é ponto máximo local     ()
     */
 
-    double calc_Critic_Point(int x, int y)
+    double calc_Critic_Point(double x, double y)
     {
 
 
     }
 
-    double derivada_x(double funcao_dx, int x, int y)
+    double derivada_x(double funcao_dx, double x, double y)
     {
         double resultado=0.0;
         bool exist=false;
@@ -43,7 +43,7 @@
         return exist;
     }
 
-    double derivada_y(double funcao_dy, int x, int y)
+    double derivada_y(double funcao_dy, double x, double y)
     {
         double resultado=0.0;
         bool exist=false;
@@ -65,26 +65,26 @@
 
     int main (void)
     {
-        int x=1;
-        int y=4;
-        int x2=2;
-        int y2=-2;
+        double x=1.0;
+        double y=4.0;
+        //double x2=2.0;
+        //double y2=-2.0;
         int verification=0;
 
         double a=0.0;
         double b=0.0;
         double c=0.0;
 
-        double funcao_dxx=(60*pow(x,4)-460*pow(x,2)+432);;
+        double funcao_dxx=(60*pow((double)x,4)-460*pow((double)x,2)+432);;
         double funcao_dxy=0.0;
-        double funcao_dyy=(6*pow(y,5)+5*pow(y,4)-202*y+200-202*pow(y,3));
+        double funcao_dyy=(6*pow((double)y,5)+5*pow((double)y,4)-202*(double)y+200-202*pow((double)y,3));
 
         double D=0.0;
         double funcao_dx=0.0;
         double funcao_dy=0.0;
 
-        funcao_dx= (12*pow(x,5)-156*pow(x,3)+432);
-        funcao_dy= (pow(y,6)+pow(y,5)-101*pow(y,2)+200*y-202*pow(y,3));
+        funcao_dx= (12*pow((double)x,5)-156*pow((double)x,3)+432);
+        funcao_dy= (pow((double)y,6)+pow((double)y,5)-101*pow((double)y,2)+200*(double)y-202*pow((double)y,3));
 
         if ( verification == derivada_x(funcao_dx, x, y) && verification == derivada_y (funcao_dy, x, y))
         {
