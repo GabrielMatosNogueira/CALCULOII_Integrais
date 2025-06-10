42984ccb4604481991e9f3a3807817ab3f0e4dcf
Gabriel Matos Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2025-06-10 12:13:30 -0300

    Create T08.c

---
+++ 02_04_2025_LAB12/T08.c
@@ -0,0 +1,50 @@
+Seja R a região do plano xy que está à direita da reta x = 4,8 e é limitada por y = 0 e pela curva x²y = 1.
+
+Determine a área da região R.
+
+|
+|
+|		|-
+|		| -
+|		|  -
+|		|   -
+|		|    ---
+-------------------------- y=0
+|		|
+|	       4,8
+
+Como resolver)
+
+Isolamos o y
+
+y=1/x^2
+b=∞
+	
+ ∞	      	     		 b			     b
+∫1/x^2 dx = 		lim     ∫1/x^2 (dx) = 		[-1/x]		(-1/4,8)-(-1/b)= (-1/4,8)+(1/b)
+4,8	    		b->∞  4,8			     4,8				    |
+												    ---> Tende a 0
+
+Quanto maior o b, menor será o numero subtraindo -1/4,8, ou seja, tenderá à um.
+≈0.208
+
+=================================================================================================================================
+
+02- x= 1,2;	y=0;	y=e^-1,1x
+
+Volume = V/π;
+
+
+|
+|
+|	|--
+|	|  --
+|	|     --
+---------------------- y = 0		
+|	|
+	x=1,2
+
+b=∞
+ ∞			 b
+∫e^-1,1x dx    =	∫e^-1,1x dx	=	
+1,2		       1,2
